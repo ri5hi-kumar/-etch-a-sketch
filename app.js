a13492f43d25f52dac2eeb0ce4861edb91b72be8
rishi-kumar <rishikumar10dec@gmail.com>
2022-11-30 08:04:13 +0530

    added slider event

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-const grid = document.querySelector('.grid');
+const DEFAULT_SIZE = 16
 
-const createGrid = () => {
-    for(let i = 1; i <= 256; i++){
+const grid = document.querySelector('.grid');
+const slider = document.querySelector('#slider');
+
+const createGrid = (size) => {
+    for(let i = 1; i <= size * size; i++){
         const box = document.createElement('div');
         box.classList.add('cell');
         grid.append(box);
+        grid.style.gridTemplateColumns = `repeat(${size}, 2fr)`;
+        grid.style.gridTemplateRows = `repeat(${size}, 2fr)`;
         box.addEventListener('mouseover', function(){
             box.style.backgroundColor = getRandomColor();
         })
     }
 }
 
-
-createGrid();
-
+slider.addEventListener('change', function(e){
+    grid.innerHTML = '';
+    let newSize = parseInt(e.target.value);
+    createGrid(newSize);
+})
 
 const getRandomColor = () =>{
     let r = Math.floor(Math.random() * 256);
@@ -21,4 +28,9 @@ const getRandomColor = () =>{
     let b = Math.floor(Math.random() * 256);
 
     return `rgb(${r}, ${g}, ${b})`
+}
+
+
+window.onload = () =>{
+    createGrid(DEFAULT_SIZE);
 }
