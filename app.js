2fdab675bfdf3cadd1749d50d373623ba4dc51b9
rishi-kumar <rishikumar10dec@gmail.com>
2022-12-01 17:30:31 +0530

    code for all buttons added

--- app.js
+++ app.js
@@ -1,25 +1,52 @@
 const DEFAULT_SIZE = 16
+const DEFAULT_MODE = 'black'
+
+let currentMode = DEFAULT_MODE;
+let currentSize = DEFAULT_SIZE;
 
 const grid = document.querySelector('.grid');
 const slider = document.querySelector('#slider');
+const clear = document.querySelector('#clear');
+const black = document.querySelector('#black');
+const rgb = document.querySelector('#rgb');
+
+function changeColor(e) {
+    if(currentMode === 'black'){
+        e.target.style.backgroundColor = 'black';
+    }
+    else if(currentMode === 'rgb'){
+        e.target.style.backgroundColor = getRandomColor();
+    }
+}
 
 const createGrid = (size) => {
+    grid.style.gridTemplateColumns = `repeat(${size}, 2fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 2fr)`;
     for(let i = 1; i <= size * size; i++){
         const box = document.createElement('div');
         box.classList.add('cell');
+        box.addEventListener('mousedown', changeColor);
         grid.append(box);
-        grid.style.gridTemplateColumns = `repeat(${size}, 2fr)`;
-        grid.style.gridTemplateRows = `repeat(${size}, 2fr)`;
-        box.addEventListener('mouseover', function(){
-            box.style.backgroundColor = getRandomColor();
-        })
     }
 }
 
 slider.addEventListener('change', function(e){
     grid.innerHTML = '';
-    let newSize = parseInt(e.target.value);
-    createGrid(newSize);
+    currentSize = parseInt(e.target.value);
+    createGrid(currentSize);
+})
+
+clear.addEventListener('click', function(){
+    grid.innerHTML = '';
+    createGrid(currentSize);
+})
+
+black.addEventListener('click', function(){
+    currentMode = 'black';
+})
+
+rgb.addEventListener('click', function(){
+    currentMode = 'rgb';
 })
 
 const getRandomColor = () =>{
